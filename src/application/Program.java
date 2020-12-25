54f0b136b1d0b14fefb16365aa580fae169b7144
Douglas Rodrigues <dougrsilv@gmail.com>
2020-12-25 20:24:51 -0300

    teste 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,9 @@ public class Program {
 	
 	public static void main(String[]args) {
 		System.out.println("teste");
+		System.out.println("teste");
+		System.out.println("teste");
+		System.out.println("teste");
 	}
 
 }
